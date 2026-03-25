c7e7c28f8c2869236aade09675243f838a6b8b1c
Manish.U <manish6singh2020@gmail.com>
2026-03-25 12:59:57 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,1 +1,15 @@
+from scipy.stats import wilcoxon
 
+def compare_all(qga, ga, pso, de):
+    results = {}
+
+    results['QGA vs GA'] = wilcoxon(qga, ga).pvalue
+    results['QGA vs PSO'] = wilcoxon(qga, pso).pvalue
+    results['QGA vs DE'] = wilcoxon(qga, de).pvalue
+
+    results['GA vs PSO'] = wilcoxon(ga, pso).pvalue
+    results['GA vs DE'] = wilcoxon(ga, de).pvalue
+
+    results['PSO vs DE'] = wilcoxon(pso, de).pvalue
+
+    return results
